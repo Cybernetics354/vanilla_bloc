69fd0523c57fdb3d424a16a5babc0c46f2f940a3
Cybernetics Core <corecybernetics354@gmail.com>
2020-09-25 21:38:45 +0700

    Event COntroller

--- lib/bloc/cybloc.dart
+++ lib/bloc/cybloc.dart
@@ -6,8 +6,12 @@ abstract class Cybloc<T, R> {
   final _streamController = new StreamController<T>.broadcast();
   Stream<T> get stream => _streamController.stream;
   StreamSink<T> get inStream => _streamController.sink;
+
+  final _eventController = new StreamController<R>.broadcast();
+  StreamSink<R> get eventController => _eventController.sink;
   
   void dispose() {
     _streamController?.close();
+    _eventController?.close();
   }
 }
