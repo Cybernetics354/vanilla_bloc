d832e5baaf4464257598fbe0e8f5d818f1e0e054
Cybernetics Core <corecybernetics354@gmail.com>
2020-09-25 21:37:31 +0700

    Hooks into stream controller

--- lib/bloc/cybloc.dart
+++ lib/bloc/cybloc.dart
@@ -1,6 +1,13 @@
 import 'dart:async';
 
+import 'dart:math';
+
 abstract class Cybloc<T, R> {
   final _streamController = new StreamController<T>.broadcast();
+  Stream<T> get stream => _streamController.stream;
+  StreamSink<T> get inStream => _streamController.sink;
   
+  void dispose() {
+    _streamController?.close();
+  }
 }
